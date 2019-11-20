478f67733d8c331261ab1b8798a6798161bc0573
icarus8050 <icarus8050@naver.com>
2019-11-20 18:44:06 +0900

    project init

---
+++ ward-shop/src/main/java/com/ward/wardshop/WardShopApplication.java
@@ -0,0 +1,13 @@
+package com.ward.wardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WardShopApplication.class, args);
+    }
+
+}
---
+++ ward-shop/src/test/java/com/ward/wardshop/WardShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ward.wardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
