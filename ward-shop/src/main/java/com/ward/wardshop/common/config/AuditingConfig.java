662a38988714b2a5f185db61ee8d5a94599cf7ec
icarus8050 <icarus8050@naver.com>
2019-12-02 21:03:04 +0900

    Auditing Config init

---
+++ ward-shop/src/main/java/com/ward/wardshop/common/config/AuditingConfig.java
@@ -0,0 +1,29 @@
+package com.ward.wardshop.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (Objects.nonNull(authentication)) {
+                return Optional.of(authentication.getName());
+            } else {
+                return Optional.of("Anonymous");
+            }
+        };
+    }
+}
