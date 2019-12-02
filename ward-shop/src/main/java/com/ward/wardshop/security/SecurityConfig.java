191413bc9bd252f436ad756b2fe0c4c50766a36d
icarus8050 <icarus8050@naver.com>
2019-12-02 20:04:50 +0900

    Security Config init

---
+++ ward-shop/src/main/java/com/ward/wardshop/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.ward.wardshop.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final Environment env;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        if (isLocalMode()) {
+            setLocalMode(http);
+        } else {
+            setProdMode(http);
+        }
+
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    private boolean isLocalMode() {
+        String profile = env.getActiveProfiles().length > 0 ? env.getActiveProfiles()[0] : "local";
+        return profile.equals("local");
+    }
+
+    private void setLocalMode(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    private void setProdMode(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated();
+    }
+}
