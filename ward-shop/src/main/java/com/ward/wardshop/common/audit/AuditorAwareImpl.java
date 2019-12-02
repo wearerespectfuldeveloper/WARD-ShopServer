5c016309692f9d9bade81034bead17921d00adef
icarus8050 <icarus8050@naver.com>
2019-12-03 00:11:10 +0900

    Entity Listener 등록 오류 수정

--- ward-shop/src/main/java/com/ward/wardshop/common/audit/AuditorAwareImpl.java
+++ ward-shop/src/main/java/com/ward/wardshop/common/audit/AuditorAwareImpl.java
@@ -1,22 +1,18 @@
 package com.ward.wardshop.common.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Objects;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
+    @NonNull
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (Objects.nonNull(authentication)) {
-            return Optional.of(authentication.getName());
-        } else {
-            return Optional.of("Anonymous");
-        }
+        return Optional.of(authentication.getName());
     }
 }
--- ward-shop/src/main/java/com/ward/wardshop/common/audit/BaseEntity.java
+++ ward-shop/src/main/java/com/ward/wardshop/common/audit/BaseEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -12,8 +13,8 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditorAwareImpl.class)
-public class BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
     @CreatedBy
     private String createdBy;
