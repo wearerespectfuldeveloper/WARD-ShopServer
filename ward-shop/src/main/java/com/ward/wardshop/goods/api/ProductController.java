bcc7b07a37f0ef7d08d8f9af2ee7264936a56ad6
icarus8050 <icarus8050@naver.com>
2020-01-25 23:04:18 +0900

    제품 정보 수정 api

--- ward-shop/src/main/java/com/ward/wardshop/goods/api/ProductController.java
+++ ward-shop/src/main/java/com/ward/wardshop/goods/api/ProductController.java
@@ -1,10 +1,12 @@
 package com.ward.wardshop.goods.api;
 
 import com.ward.wardshop.goods.api.model.ProductForm;
+import com.ward.wardshop.goods.api.model.ProductUpdateForm;
 import com.ward.wardshop.goods.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,5 +25,10 @@ public class ProductController {
     public Long createProduct(@Valid ProductForm productForm, MultipartFile imgFile) throws IOException {
 
         return productService.create(productForm, imgFile);
+    }
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Long updateProduct(@Valid ProductUpdateForm form, MultipartFile imgFile) throws IOException {
+        return productService.update(form, imgFile);
     }
 }
