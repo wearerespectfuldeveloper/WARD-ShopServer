1e8013f5676cc8585973c2002893b15f4a02e657
insunNicole <florachoi1124@naver.com>
2020-02-22 18:35:00 +0900

    add create and update code to controller

--- ward-shop/src/main/java/com/ward/wardshop/goods/api/ProductDetailComponentController.java
+++ ward-shop/src/main/java/com/ward/wardshop/goods/api/ProductDetailComponentController.java
@@ -1,16 +1,29 @@
 package com.ward.wardshop.goods.api;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.ward.wardshop.goods.api.model.ProductForm;
+import com.ward.wardshop.goods.api.model.ProductUpdateForm;
 import com.ward.wardshop.goods.service.ProductDetailComponentService;
 import com.ward.wardshop.goods.service.dto.ComponentDto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,12 +36,26 @@ public class ProductDetailComponentController {
         return productDetailComponentService.getProductDetailComponents(productIdx);
     }
 
+    @PostMapping(path="/product/{productIdx}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Long createComponent(@Valid ProductForm productForm, MultipartFile imgFile, Long productIdx) throws IOException {
+
+        return productDetailComponentService.createComponent(productIdx, productForm, imgFile);
+    }
+
     @PutMapping("/product/{productIdx}/components/{componentIdx}")
     public ResponseEntity<String> moveComponent(@PathVariable Long productIdx,
                                                 @PathVariable Long componentIdx,
                                                 SequenceRequest sequenceRequest) {
         productDetailComponentService.moveComponent(productIdx, componentIdx, sequenceRequest.getSequence());
         return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/product/{productIdx}/components/{componentIdx}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Long updateComponent(
+            @PathVariable Long productIdx,
+            @Valid ProductUpdateForm form,
+            MultipartFile imgFile) throws IOException {
+        return productDetailComponentService.updateComponent(productIdx, form, imgFile);
     }
 
     @DeleteMapping("/product/{productIdx}/components/{componentIdx}")
